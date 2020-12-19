23d4fd6e5444eb9622f85b17b20a57f5ce447ada
Josh Carter <joshcarter8400@gmail.com>
2020-12-19 12:21:27 -0500

    added about rendering

---
+++ src/components/About/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function About() {
+  return (
+    <div class="d-flex flex-column">
+      <div class="profile">
+        <h1 class="text-light">Josh Carter</h1>
+        {/* <div class="social-links mt-3 text-center">
+          <a
+            href="https://github.com/JoshCarter8400"
+            target="blank"
+            class="github"
+          >
+            <i class="bx bxl-github"></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/joshcarter4813"
+            target="blank"
+            class="linkedin"
+          >
+            <i class="bx bxl-linkedin"></i>
+          </a>
+        </div> */}
+      </div>
+    </div>
+  );
+}
+
+export default About;
