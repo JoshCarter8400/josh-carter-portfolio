2bb0351edcb02d24f592f0e8d090fc1280d4fb9d
Josh Carter <joshcarter8400@gmail.com>
2020-12-23 13:10:30 -0500

    added gh-pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./components/About";
 import Footer from "./components/Footer";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
+import Resume from "./components/Resume";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
 
       <About />
       <Portfolio />
+      <Resume />
       <Contact />
       <Footer />
     </div>
