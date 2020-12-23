0d4137d47acf0a96d67ad0ceaf8d15a7ae9e07a2
Josh Carter <joshcarter8400@gmail.com>
2020-12-23 16:55:55 -0500

    tried to add another skills section

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,80 +3,85 @@ import React from "react";
 function Resume() {
   return (
     <main id="main">
-      <section id="about" class="about">
-        <div class="container">
-          <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
-            <div class=" about content">
+      <section id="about" className="about">
+        <div className="container">
+          <div className="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <div className=" about content">
               <h3>Skills</h3>
             </div>
-            <div class="row">
-              <div class="col-lg-6">
+            <div className="row">
+              <div className="col-lg-6">
                 <ul>
                   <li>
-                    <i class="icofont-rounded-right"></i> <strong>HTML5</strong>
+                    <i className="icofont-rounded-right"></i>{" "}
+                    <strong>HTML5</strong>
                   </li>
                   <li>
-                    <i class="icofont-rounded-right"></i> <strong>CSS3</strong>
+                    <i className="icofont-rounded-right"></i>{" "}
+                    <strong>CSS3</strong>
                   </li>
                   <li>
-                    <i class="icofont-rounded-right"></i>{" "}
+                    <i className="icofont-rounded-right"></i>{" "}
                     <strong>Bootstrap</strong>
                   </li>
                   <li>
-                    <i class="icofont-rounded-right"></i> <strong>React</strong>
+                    <i className="icofont-rounded-right"></i>{" "}
+                    <strong>React</strong>
                   </li>
                   <li>
-                    <i class="icofont-rounded-right"></i>{" "}
+                    <i className="icofont-rounded-right"></i>{" "}
                     <strong>JavaScript ES6</strong>
                   </li>
                   <li>
-                    <i class="icofont-rounded-right"></i>{" "}
+                    <i className="icofont-rounded-right"></i>{" "}
                     <strong>jQuery</strong>
                   </li>
                   <li>
-                    <i class="icofont-rounded-right"></i>{" "}
+                    <i className="icofont-rounded-right"></i>{" "}
                     <strong>Handlebars</strong>
                   </li>
                   <li>
-                    <i class="icofont-rounded-right"></i>{" "}
+                    <i className="icofont-rounded-right"></i>{" "}
                     <strong>RESTful APIs</strong>
                   </li>
                   <li>
-                    <i class="icofont-rounded-right"></i>{" "}
+                    <i className="icofont-rounded-right"></i>{" "}
                     <strong>Progressive Web Apps</strong>
                   </li>
                 </ul>
               </div>
-              <div class="col-lg-6">
+              <div className="col-lg-6">
                 <ul>
                   <li>
-                    <i class="icofont-rounded-right"></i> <strong>Node</strong>
+                    <i className="icofont-rounded-right"></i>{" "}
+                    <strong>Node</strong>
                   </li>
                   <li>
-                    <i class="icofont-rounded-right"></i>{" "}
+                    <i className="icofont-rounded-right"></i>{" "}
                     <strong>Express</strong>
                   </li>
                   <li>
-                    <i class="icofont-rounded-right"></i>{" "}
+                    <i className="icofont-rounded-right"></i>{" "}
                     <strong>Mongoose</strong>
                   </li>
                   <li>
-                    <i class="icofont-rounded-right"></i>{" "}
+                    <i className="icofont-rounded-right"></i>{" "}
                     <strong>Sequelize</strong>
                   </li>
                   <li>
-                    <i class="icofont-rounded-right"></i> <strong>MySQL</strong>
+                    <i className="icofont-rounded-right"></i>{" "}
+                    <strong>MySQL</strong>
                   </li>
                   <li>
-                    <i class="icofont-rounded-right"></i>{" "}
+                    <i className="icofont-rounded-right"></i>{" "}
                     <strong>Inquirer</strong>
                   </li>
                   <li>
-                    <i class="icofont-rounded-right"></i>{" "}
+                    <i className="icofont-rounded-right"></i>{" "}
                     <strong>Object-Oriented Programming</strong>
                   </li>
                   <li>
-                    <i class="icofont-rounded-right"></i>{" "}
+                    <i className="icofont-rounded-right"></i>{" "}
                     <strong>Model-View-Controller Paradigm</strong>
                   </li>
                 </ul>
